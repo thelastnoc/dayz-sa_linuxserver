f280dab482caa67499bdd71115549041af512548
thelastnoc <thelastnoc@users.noreply.github.com>
2018-10-04 21:33:42 +0200

    Update dayz_setup.sh

--- script/dayz_setup.sh
+++ script/dayz_setup.sh
@@ -1,20 +1,29 @@
-WIP
+# WIP
+# the script needs root or sudo privileges
+if [ "$(whoami)" != "root" ] || [ "sudo -v" != "" ]; then
+  printf "[ FAIL ] You have to run this script as root!\n"
+  exit 1
+fi
 
-# debian 9
 sudo dpkg --add-architecture i386
-wget -nc https://dl.winehq.org/wine-builds/Release.key | sudo apt-key add Release.key
-printf "deb https://dl.winehq.org/wine-builds/debian/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/winehq.source.list > /dev/null
+# get the distribution of the os
+distroid=$(grep ID /etc/os-release | grep -v _ID | grep -v ID_ | sed 's/ID=//g' | sed 's/\"//g')
+
+# debian and ubuntu will pass
+if [ "${distroid}" == "debian" ] || [ "${distroid}" == "ubuntu" ]; then
+  wget -nc https://dl.winehq.org/wine-builds/Release.key | sudo apt-key add Release.key
+  printf "deb https://dl.winehq.org/wine-builds/${distroid}/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/winehq.source.list > /dev/null
+else
+  printf " [ INFO ] Your OS / Distribution wasn't tested yet.\n"
+if [ "${distroid}" == "ubuntu" ]; then
+  # ubuntu
+  sudo add-apt-repository universe
+fi
+
+# install required packages
 sudo apt install apt-transport-https -y
+sudo apt update
+sudo apt install --install-recommends winehq-stable xvfb winbind tmux nano htop curl lib32gcc1 libstdc++6 libstdc++6:i386 psmisc -y
 
-
-# ubuntu
-sudo add-apt-repository universe && sudo apt update
-sudo dpkg --add-architecture i386
-wget -nc https://dl.winehq.org/wine-builds/Release.key | sudo apt-key add Release.key
-printf "deb https://dl.winehq.org/wine-builds/ubuntu/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/winehq.source.list > /dev/null
-sudo apt install apt-transport-https -y
-
-# packs
-sudo apt update && sudo apt install --install-recommends winehq-stable xvfb winbind tmux nano htop curl lib32gcc1 libstdc++6 libstdc++6:i386 psmisc -y
-
+# add a new user for the gameserver
 sudo useradd -m -s /bin/bash dayz && sudo su - dayz
