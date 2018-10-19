cf6f2df9463835b6db66a1c21619a3ab5701704c
thelastnoc <thelastnoc@users.noreply.github.com>
2018-10-19 18:20:57 +0200

    add archlinux

--- script/dayz_setup.sh
+++ script/dayz_setup.sh
@@ -5,29 +5,38 @@ if [ "$(sudo -v)" != "" ]; then
         exit 1
 fi
 
-sudo dpkg --add-architecture i386
 # get the distribution of the os
 distroid=$(grep ID /etc/os-release | grep -v _ID | grep -v ID_ | sed 's/ID=//g' | sed 's/\"//g')
-
-# debian and ubuntu will pass
+# debian and ubuntu
 if [ "${distroid}" == "debian" ] || [ "${distroid}" == "ubuntu" ]; then
         wget -nc https://dl.winehq.org/wine-builds/Release.key
         sleep 0.5
         sudo apt-key add Release.key
         printf "deb https://dl.winehq.org/wine-builds/${distroid}/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/winehq.source.list > /dev/null
+        # ubuntu serves needs universe repo
+        if [ "${distroid}" == "ubuntu" ]; then
+                sudo add-apt-repository universe
+        fi
+        # install required packages
+        sudo dpkg --add-architecture i386
+        sudo apt install apt-transport-https -y
+        sudo apt update
+        sudo apt install --install-recommends winehq-stable xvfb winbind tmux nano htop curl lib32gcc1 libstdc++6 libstdc++6:i386 psmisc wget -y
+# archlinux
+elif [ "${distroid}" == "arch" ]; then
+        # enable multilib
+        mlibln=$(grep -nr multilib] pacman.conf | cut -d : -f 1)
+        sudo sed -i "${mlibln}s/^#//g" /etc/pacman.conf
+        mlibln=$(($mlibln + 1))
+        sudo sed -i "${mlibln}s/^#//g" /etc/pacman.conf
+        sudo pacman -Syu
+        sudo pacman -S wine xvfb tmux nano htop curl psmisc wget lib32-gcc-libs lib32-libstdc++5
+if
+
 else
         printf "[ INFO ] Your Linux Distribution is not tested yet.\n"
 fi
 
-# ubuntu serves needs universe repo
-if [ "${distroid}" == "ubuntu" ]; then
-        sudo add-apt-repository universe
-fi
-
-# install required packages
-sudo apt install apt-transport-https -y
-sudo apt update
-sudo apt install --install-recommends winehq-stable xvfb winbind tmux nano htop curl lib32gcc1 libstdc++6 libstdc++6:i386 psmisc -y
 
 # add a new user for the gameserver
 sudo useradd -m -s /bin/bash dayz
