b6f86294b3fae7bbc6291b1cc455ba8bb0c80f13
thelastnoc <thelastnoc@users.noreply.github.com>
2021-09-10 22:04:16 +0200

    Update dayz_setup.sh

--- script/dayz_setup.sh
+++ script/dayz_setup.sh
@@ -7,11 +7,7 @@ fi
 # get the distribution of the os
 distroid=$(grep ID /etc/os-release | grep -v _ID | grep -v ID_ | sed 's/ID=//g' | sed 's/\"//g')
 # debian and ubuntu
-if [ "${distroid}" == "debian" ] || [ "${distroid}" == "ubuntu" ]; then
-        wget -nc https://dl.winehq.org/wine-builds/winehq.key
-        sleep 0.5
-        sudo apt-key add winehq.key
-        printf "deb https://dl.winehq.org/wine-builds/${distroid}/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/winehq.source.list > /dev/null
+if [ "${distroid}" == "debian" ] || [ "${distroid}" == "ubuntu" || [ "${distroid}" == "pop" ]; then
         # ubuntu 18 server needs universe repo
         if [ "${distroid}" == "ubuntu" ]; then
                 sudo apt install software-properties-common -y
@@ -21,7 +17,8 @@ if [ "${distroid}" == "debian" ] || [ "${distroid}" == "ubuntu" ]; then
         sudo dpkg --add-architecture i386
         sudo apt install apt-transport-https -y
         sudo apt update
-        sudo apt install --install-recommends winehq-stable xvfb winbind tmux nano htop curl lib32gcc1 libstdc++6 libstdc++6:i386 psmisc wget -y
+        sudo apt install xvfb tmux nano htop curl lib32gcc1 libstdc++6 libstdc++6:i386 psmisc wget -y
+
 # archlinux
 elif [ "${distroid}" == "arch" ]; then
         sudo sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
@@ -32,7 +29,7 @@ elif [ "${distroid}" == "arch" ]; then
         mlibln=$(($mlibln + 1))
         sudo sed -i "${mlibln}s/^#//g" /etc/pacman.conf
         sudo pacman -Syu
-        sudo pacman -S wine xorg-server-xvfb tmux nano htop curl psmisc wget lib32-gcc-libs extra/libglvnd multilib/lib32-libglvnd
+        sudo pacman -S xorg-server-xvfb tmux nano htop curl psmisc wget lib32-gcc-libs extra/libglvnd multilib/lib32-libglvnd
 else
         printf "[ INFO ] Your Linux Distribution is not tested yet.\n"
 fi
